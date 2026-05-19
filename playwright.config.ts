0af7fc38893eaa3ebb271524b390481cb5fcdcf2
Maulik <maulik1004@gmail.com>
2026-05-19 16:16:21 +0530

    added api test cases and also modified some files

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
     video : 'retain-on-failure',  //Capture video
 
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+     baseURL: 'https://restful-booker.herokuapp.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'off',
